bf399953ce8b8026edc2fe91626c6c0e7e4058a4
Vasudev Sharma <sharma.vdv@gmail.com>
2022-12-14 16:51:03 +0530

    final

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,41 +1,39 @@
-import {useEffect} from 'react'
-import {useNavigate} from 'react-router-dom'
-import {useSelector, useDispatch} from 'react-redux'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm'
-import Spinner from '../components/Spinner'
-import {getGoals, reset} from '../features/goals/goalSlice'
 import GoalItem from '../components/GoalItem'
+import Spinner from '../components/Spinner'
+import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
-
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
-  const {user} = useSelector((state) => state.auth)
-  const {goals, isLoading, isError, message} = useSelector((state) => state.goals)
-
+  const { user } = useSelector((state) => state.auth)
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  )
 
   useEffect(() => {
-
-    if(isError) {
-      console.log(message);
+    if (isError) {
+      console.log(message)
     }
-    if(!user) {
+ 
+    if (!user) {
       navigate('/login')
-
     }
+
     dispatch(getGoals())
 
     return () => {
       dispatch(reset())
     }
-
   }, [user, navigate, isError, message, dispatch])
 
-  if(isLoading) {
-    return <Spinner/>
+  if (isLoading) {
+    return <Spinner />
   }
-
 
   return (
     <>
@@ -43,18 +41,21 @@ function Dashboard() {
         <h1>Welcome {user && user.name}</h1>
         <p>Goals Dashboard</p>
       </section>
-      <GoalForm/>
+
+      <GoalForm />
 
       <section className='content'>
         {goals.length > 0 ? (
           <div className='goals'>
-            {goals.map((goal) => (<GoalItem key={goal._id} goal={goal}/>) )}
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
           </div>
-        ) : (<h3>You have not set any goals</h3>)}
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
       </section>
-
     </>
-
   )
 }
 
